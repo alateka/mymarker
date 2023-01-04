64eca927544657fc55f168e27dfb090559316ba5
ALATEKA <alateka@vivaldi.net>
2023-01-05 00:04:36 +0100

    add: launcher icon

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,12 @@ class MyMarker extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MyMarker',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        scaffoldBackgroundColor: Colors.green
-      ),
-      // darkTheme: ThemeData.dark(),
-      home: const CartPage()
-    );
+        title: 'MyMarker',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+          scaffoldBackgroundColor: Colors.white,
+        ),
+        // darkTheme: ThemeData.dark(),
+        home: const CartPage());
   }
 }
--- lib/views/cart_page.dart
+++ lib/views/cart_page.dart
@@ -16,14 +16,13 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
-
   final List<CartItem> cart = [];
   final MarketStorage storage = MarketStorage();
 
   @override
   void initState() {
     super.initState();
-    
+
     storage.readFile().then((dataFile) {
       for (var item in jsonDecode(dataFile)) {
         setState(() {
@@ -38,110 +37,93 @@ class _CartPageState extends State<CartPage> {
       cart.removeAt(id);
     });
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("MyMarker"),
-        actions: [
-          PopupMenuButton(itemBuilder: (context) {
-            return [
-              const PopupMenuItem<int>(
+        appBar: AppBar(
+          title: const Text("MyMarker"),
+          actions: [
+            PopupMenuButton(itemBuilder: (context) {
+              return [
+                const PopupMenuItem<int>(
                   value: 0,
                   child: Text("Sobre la APP"),
-              ),
-            ];
-          },
-          onSelected: (value) {
-            if( value == 0 ) {
-              // About
-            }
-          })
-        ],
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-          image: Image.asset('assets/background_blur.jpg').image,
-          fit: BoxFit.cover
-          )
+                ),
+              ];
+            }, onSelected: (value) {
+              if (value == 0) {
+                // About
+              }
+            })
+          ],
         ),
-        child: ListView.builder(
-          itemCount: cart.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Padding(
-              padding: const EdgeInsets.fromLTRB(9, 13, 9, 0),
+        body: ListView.builder(
+            itemCount: cart.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding: const EdgeInsets.fromLTRB(9, 13, 9, 0),
                 child: Container(
-                  decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 55, 145, 43),
-                  borderRadius: BorderRadius.circular(9),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.black,
-                      blurRadius: 11,
-                      offset: Offset(0,5)
-                      )
-                    ]
-                  ),
-                  child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.circular(19),
-                  ),
-                  margin: const EdgeInsets.all(5.0),
-                  alignment: Alignment.center,
-                  child: ListTile(
-                    leading: SvgPicture.asset(
-                      autoIcon(cart[index].name),
-                      width: 39,
-                      height: 39,
-                    ),
-                    title: Text(cart[index].name, style: const TextStyle(color: Colors.white)),
-                    trailing: PopupMenuButton<int>(
-                      icon: const Icon(Icons.menu, color: Colors.white),
-                      itemBuilder: (context) {
-                        return [
-                          const PopupMenuItem(
-                            value: 1,
-                            child: Text('Eliminar'),
-                          )
-                        ];
-                      },
-                      onSelected: (int value) {
-                        if ( value == 1 ) {
-                          deleteItem(index);
-                          writeChanges(storage, cart);
-                        }
-                      },
-                    )
-                  )
-                )
-              ),
-            );
-          }
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: const Color.fromARGB(255, 55, 145, 43),
-        shape: RoundedRectangleBorder(
-          side: const BorderSide(
-            width: 5,
-            color: Colors.green,
-          ),
-          borderRadius: BorderRadius.circular(100)
-        ),
-        onPressed: () => Navigator.push(
-          context, 
-          MaterialPageRoute(
-            builder: (context) => AddItemPage(
-              storage: storage,
-              cart: cart)
-            ))
-          .then((value){setState(() {});}),
-        child: const Icon(Icons.add)
-      )
-    );
+                    decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 55, 145, 43),
+                        borderRadius: BorderRadius.circular(9),
+                        boxShadow: const [
+                          BoxShadow(
+                              color: Colors.black,
+                              blurRadius: 11,
+                              offset: Offset(0, 5))
+                        ]),
+                    child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: BorderRadius.circular(19),
+                        ),
+                        margin: const EdgeInsets.all(5.0),
+                        alignment: Alignment.center,
+                        child: ListTile(
+                            leading: SvgPicture.asset(
+                              autoIcon(cart[index].name),
+                              width: 39,
+                              height: 39,
+                            ),
+                            title: Text(cart[index].name,
+                                style: const TextStyle(color: Colors.white)),
+                            trailing: PopupMenuButton<int>(
+                              icon: const Icon(Icons.menu, color: Colors.white),
+                              itemBuilder: (context) {
+                                return [
+                                  const PopupMenuItem(
+                                    value: 1,
+                                    child: Text('Eliminar'),
+                                  )
+                                ];
+                              },
+                              onSelected: (int value) {
+                                if (value == 1) {
+                                  deleteItem(index);
+                                  writeChanges(storage, cart);
+                                }
+                              },
+                            )))),
+              );
+            }),
+        floatingActionButton: FloatingActionButton(
+            backgroundColor: const Color.fromARGB(255, 55, 145, 43),
+            shape: RoundedRectangleBorder(
+                side: const BorderSide(
+                  width: 5,
+                  color: Colors.green,
+                ),
+                borderRadius: BorderRadius.circular(100)),
+            onPressed: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                AddItemPage(storage: storage, cart: cart)))
+                    .then((value) {
+                  setState(() {});
+                }),
+            child: const Icon(Icons.add)));
   }
 
   Future<File> writeChanges(MarketStorage storage, List<CartItem> cart) async {
